bbd829650b2fb97c24c14b5c371bdba2e1f33436
桃夭 <920375825@qq.com>
2019-07-18 21:23:05 +0800

    modify app.js and imag

--- login/app.js
+++ login/app.js
@@ -1,11 +1,28 @@
+
 // 引入express文件
 const express=require("express");
 //引入路由
-const loginRouter=require("./router/login")
+const loginRouter=require("./router/login");
 // 引入第三方插件
 const bodyPaser=require("body-parser");
+//引入跨越
+const cors = require("cors");  
+//引入session
+// const session=require("express-session");
 // 创建服务器
 var server=express();
+//跨越
+server.use(cors({
+    origin:["http://127.0.0.1:5500",
+    "http://localhost:5500"],
+    credentials:true
+}));
+//session
+// server.use(session({
+//     secret:"128位字符串",
+//     resave:true,
+//     saveUninitialized:true
+//   }))
 // 设置端口
 server.listen(8080);
 // 托管静态资源到public下
